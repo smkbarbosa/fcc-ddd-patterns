f55560b93dce9118d2e11311f6ca88a362ebada4
Samuel Barbosa <contato@smkbarbosa.eti.br>
2022-12-27 16:40:21 -0300

    sobre consistencias...e autovalidacao

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,25 @@
+// a entidade sempre vai ter que representar o estado correto do elemento
+// uma entidade por padrão sempre tem que se autovalidar
 class Customer{
   _id:string;
   _name:string;
   _address:string;
-  _active:boolean = true;
+  _active:boolean = false;
 
   constructor(id: string, name: string, address: string){
     this._id = id;
     this._name = name;
-    this._address = address;
+    this.validade();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error("Name is required");
+    }
+
+    if (this._id.length === 0) {
+      throw new Error("Id is required");
+    }
   }
 
   // Ao inves de usar getters and setters no dominio da apĺicação, usar funções que tenham relação com a regra de negócio
@@ -17,6 +29,9 @@ class Customer{
   }
 
   activate() {
+    if (this._address.length === 0) {
+      throw new Error("Address is mandatory to activate a customer");
+    }
     this._active = true;
   }
 
